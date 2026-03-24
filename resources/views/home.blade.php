0f5becd214d40e70ccdcde27726998847055d0d2
rawan202102421 <rawan3almahoozi@gmail.com>
2026-03-24 19:57:47 +0300

    Homepage draft created

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <x-slot:title>Home Page</x-slot:title>
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">Welcome to UniGuide!</h1>
+                    <p class="mt-4 text-base-content/60">....</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+//it says when someone visits / route them to the welcome 
+// page which is in the view folder
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
